d0aeb5aaf6384cb7fd31b8c51217c9c7d0f9703d
Anas-Mughal <110368394+Anas-Mughal@users.noreply.github.com>
2024-06-09 13:43:46 +0000

    "Third Commit"

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types";
 import "./globals.css";
-import MainBackground from "./Components/mainbackground"; // Component name should start with a capital letter
+import MainBackground from "./Components/mainbackground"; // Ensure file name matches the import
 import Header from "./Components/Header";
 import FooterContent from "./Components/Footer";
+import Head from "next/head";
 
 export const metadata = {
   title: "Glacier",
@@ -12,11 +13,16 @@ export const metadata = {
 function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="../public/LOGO.ico" />
+      </Head>
       <body className="relative z-[0] bg-body-bg-color">
         <MainBackground />
-        <Header></Header>
+        <Header />
         {children}
-        <FooterContent></FooterContent>
+        <FooterContent />
       </body>
     </html>
   );
