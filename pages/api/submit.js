5a8f0c8590c783b2e1b8c8a8ec198798d9fcbca4
Anas-Mughal <110368394+Anas-Mughal@users.noreply.github.com>
2024-06-18 10:07:00 +0000

    "Commit with appwrite"

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,34 +1,37 @@
 // pages/api/submit.js
-import { connectDatabase, getDb } from "../../lib/mongodb";
+import { databases } from "../../lib/appwrite";
+import { ID } from "appwrite";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { name, email, number, bottles, review } = req.body;
 
     try {
-      await connectDatabase();
+      const databaseId = "66714a2f0008fd332708"; // Replace with your Database ID
+      const collectionId = "66714b930021775a8df2"; // Replace with your Collection ID
 
-      const db = getDb();
-
-      const result = await db
-        .collection("formdata_collection_glacier")
-        .insertOne({
+      const response = await databases.createDocument(
+        databaseId,
+        collectionId,
+        ID.unique(), // Automatically generate a unique document ID
+        {
           name,
           email,
           number,
           bottles,
           review,
-        });
+        }
+      );
 
       res.status(200).json({
         message: "Message sent successfully!",
-        insertedId: result.insertedId,
+        documentId: response.$id,
       });
     } catch (error) {
       console.error("Error sending message:", error);
-      res
-        .status(500)
-        .json({ message: "Failed to send message. Please try again later." });
+      res.status(500).json({
+        message: "Failed to send message. Please try again later.",
+      });
     }
   } else {
     res.status(405).json({ message: "Method Not Allowed" });
