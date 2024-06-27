8df18e3de4fca1ebdc45b6b00c1d93a0836178bc
Anas-Mughal <tagheiranas@gmail.com>
2024-06-28 03:01:23 +0500

    "Some changes - 2"

--- app/about/AboutSections/WaterQualityTable.jsx
+++ app/about/AboutSections/WaterQualityTable.jsx
@@ -4,87 +4,87 @@ function WaterQualityTable() {
       <h1 className="text-nowrap text-[7.5vw]">
         MINERALS <span className="text-primary">COMPOSITION</span>
       </h1>
-      <table className=" border-collapse  bg-[#ffffff1c] backdrop-blur-lg rounded-[0.75vw]table-auto">
+      <table className=" border-collapse  bg-[#ffffff1c] backdrop-blur-lg rounded-[0.75vw] table-auto overflow-hidden border border-[#ffffff24] ">
         <thead>
           <tr>
-            <th className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw]  border-[#ffffff24]">
-              <p>Parameter</p>
+            <th className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]border-t border-r border-[#ffffff24]">
+              <p className="text-primary">Parameter</p>
             </th>
-            <th className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw]  border-[#ffffff24]">
-              <p>Range (MG/L)</p>
+            <th className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]border-t border-r border-[#ffffff24]">
+              <p className="text-primary">Range (MG/L)</p>
             </th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw] pl-[3vw] border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   text-center  border-t border-r border-[#ffffff24]">
               <p>Calcium</p>
             </td>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw]  flex justify-center items-center  border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   flex justify-center items-center  border-t border-l border-[#ffffff24]">
               <p>35-75 MG/L</p>
             </td>
           </tr>
           <tr>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw] pl-[3vw] border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   text-center  border-t border-r border-[#ffffff24]">
               <p>Magnesium</p>
             </td>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw]  flex justify-center items-center  border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   flex justify-center items-center  border-t border-l border-[#ffffff24]">
               <p>05-30 MG/L</p>
             </td>
           </tr>
           <tr>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw] pl-[3vw] border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   text-center  border-t border-r border-[#ffffff24]">
               <p>Potassium</p>
             </td>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw]  flex justify-center items-center  border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   flex justify-center items-center  border-t border-l border-[#ffffff24]">
               <p>0.1-05 MG/L</p>
             </td>
           </tr>
           <tr>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw] pl-[3vw] border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   text-center  border-t border-r border-[#ffffff24]">
               <p>Sulphate</p>
             </td>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw]  flex justify-center items-center  border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   flex justify-center items-center  border-t border-l border-[#ffffff24]">
               <p>15-45 MG/L</p>
             </td>
           </tr>
           <tr>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw] pl-[3vw] border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   text-center  border-t border-r border-[#ffffff24]">
               <p>Bicarbonate</p>
             </td>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw]  flex justify-center items-center  border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   flex justify-center items-center  border-t border-l border-[#ffffff24]">
               <p>15-45 MG/L</p>
             </td>
           </tr>
           <tr>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw] pl-[3vw] border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   text-center  border-t border-r border-[#ffffff24]">
               <p>Sodium</p>
             </td>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw]  flex justify-center items-center  border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   flex justify-center items-center  border-t border-l border-[#ffffff24]">
               <p>05-30 MG/L</p>
             </td>
           </tr>
           <tr>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw] pl-[3vw] border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   text-center  border-t border-r border-[#ffffff24]">
               <p>Chloride</p>
             </td>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw]  flex justify-center items-center  border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   flex justify-center items-center  border-t border-l border-[#ffffff24]">
               <p>50-100 MG/L</p>
             </td>
           </tr>
           <tr>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw] pl-[3vw] border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   text-center  border-t border-r border-[#ffffff24]">
               <p>TDS</p>
             </td>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw]  flex justify-center items-center  border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   flex justify-center items-center  border-t border-l border-[#ffffff24]">
               <p>150-250 MG/L</p>
             </td>
           </tr>
           <tr>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw] pl-[3vw] border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   text-center  border-t border-r border-[#ffffff24]">
               <p>pH</p>
             </td>
-            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw] border-[0.1vw]  flex justify-center items-center  border-[#ffffff24]">
+            <td className="w-[20.052vw] h-[3.125vw] max-[950px]:w-[40vw] max-[950px]:h-[7.5vw]   flex justify-center items-center  border-t border-l border-[#ffffff24]">
               <p>6.5-8 MG/L</p>
             </td>
           </tr>
--- app/home/HomeSections/Section6.jsx
+++ app/home/HomeSections/Section6.jsx
@@ -14,32 +14,38 @@ function Section6() {
       <h1>
         CONTACT <span className="text-primary">INFO</span>
       </h1>
-      <table className="border-collapse bg-[#ffffff1c] backdrop-blur-lg rounded-[0.75vw] table-auto overflow-hidden border border-[#ffffff24]">
+      <table className="border-collapse bg-[#ffffff1c] backdrop-blur-lg rounded-[0.75vw] table-auto overflow-hidden border border-[#ffffff24] w-full max-[950px]:w-[90dvw]">
         <tbody>
           <tr>
-            <td className="w-[20.052vw] h-[3.125vw] border-t border-r border-[#ffffff24] px-4 py-2">
-              <p className="text-nowrap">
-                <span className="text-primary">Phone:</span> &nbsp;&nbsp; 0300 -
-                XXXXXXX
-              </p>
-            </td>
+            <th className="w-[20.052vw] h-[3.125vw] border-t border-r border-[#ffffff24] px-4 py-2">
+              <p className="text-left text-nowrap text-primary pl-[12.5%]">Phone:</p>
+            </th>
             <td className="w-[20.052vw] h-[3.125vw] border-t border-l border-[#ffffff24] px-4 py-2">
-              <p className="pwrap">
-                <span className="text-primary">Address:</span> Main Road X,
-                Walton, Lahore
-              </p>
+              <p className="pwrap">0300 - XXXXXXX</p>
             </td>
           </tr>
           <tr>
-            <td className="w-[20.052vw] h-[3.125vw] border-t border-r border-[#ffffff24] px-4 py-2">
-              <p className="text-nowrap">
-                <span className="text-primary">Phone 2:</span> 0300 - XXXXXXX
-              </p>
-            </td>
+            <th className="w-[20.052vw] h-[3.125vw] border-t border-r border-[#ffffff24] px-4 py-2">
+              <p className="text-left text-nowrap text-primary pl-[12.5%]">Phone 2:</p>
+            </th>
             <td className="w-[20.052vw] h-[3.125vw] border-t border-l border-[#ffffff24] px-4 py-2">
-              <p className="text-nowrap">
-                <span className="text-primary">Email:</span> example@gmail.com
-              </p>
+              <p className="pwrap">0300 - XXXXXXX</p>
+            </td>
+          </tr>
+          <tr>
+            <th className="w-[20.052vw] h-[3.125vw] border-t border-r border-[#ffffff24] px-4 py-2">
+              <p className="text-left text-nowrap text-primary pl-[12.5%]">Address:</p>
+            </th>
+            <td className="w-[20.052vw] h-[3.125vw] border-t border-l border-[#ffffff24] px-4 py-2">
+              <p className="pwrap"> Main Road X, Walton, Lahore Main Road X, </p>
+            </td>
+          </tr>
+          <tr>
+            <th className="w-[20.052vw] h-[3.125vw] border-t border-r border-[#ffffff24] px-4 py-2">
+              <p className="text-left text-nowrap text-primary pl-[12.5%]">Email:</p>
+            </th>
+            <td className="w-[20.052vw] h-[3.125vw] border-t border-l border-[#ffffff24] px-4 py-2">
+              <p className="pwrap">example@gmail.com</p>
             </td>
           </tr>
         </tbody>
